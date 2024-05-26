cdbab32e1479724233dedbafa8241c7f3c26e872
yusuf <yusufgad24@gmail.com>
2024-05-26 17:35:30 +0300

    Adding Page Metadata and Favicon

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About",
+};
+
 function Page() {
   return <div>about page</div>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,29 @@
-export const metadata = {
-  title: "The Wild Oasis",
-  description: "A beautiful oasis in the wild",
-};
-
+import "@/app/_styles/globals.css";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+
+export const metadata = {
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description: "Luxurious cabins in the heart of the wilderness",
+};
 
 // children prop will always be the current page component
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header style={{ display: "flex", gap: "14px" }}>
+      <body className="bg-primary-950  text-primary-100 min-h-screen">
+        <header>
           <Logo />
           <Navigation />
         </header>
 
         <main>{children}</main>
 
-        <footer style={{ marginTop: "32px" }}>
-          Copyright by the Wild Oasis
-        </footer>
+        <footer>Copyright by the Wild Oasis</footer>
       </body>
     </html>
   );
