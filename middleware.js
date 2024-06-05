120e1c2aa21d8855fdbe76f32d05b15d4552a2c4
yusuf <yusufgad24@gmail.com>
2024-06-05 11:29:29 +0300

    Building a Custom Sign Out Button

--- middleware.js
+++ middleware.js
@@ -1,5 +1,3 @@
-// import { NextResponse } from "next/server";
-
 // export function middleware(request) {
 //   console.log(request);
 
@@ -9,9 +7,25 @@
 // // configure all the routes which will be this middleware should work
 
 import { auth } from "@/app/_lib/auth";
+import { NextResponse } from "next/server";
 
-export const middleware = auth;
+// export const middleware = auth;
 
-export const config = {
-  matcher: ["/account"],
-};
+// export const config = {
+//   matcher: ["/account"],
+// };
+
+export async function middleware(req) {
+  const session = await auth();
+  const { pathname } = req.nextUrl;
+
+  if (pathname === "/login" && session?.user) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (pathname.startsWith("/account") && !session?.user) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
